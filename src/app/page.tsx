3a3c2f9b143e5738342505055dd940b7e343aae7
Keshav Prasad <keshavssp04@gmail.com>
2025-08-16 23:41:50 +0530

    navbar addition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-'use client'
-import ShareButton from '@/components/ui/share-button'
-import WrapButton from '@/components/ui/wrap-button'
-import { Banana, Cloud, LandPlot, Twitter } from 'lucide-react'
+import Navbar from '@/components/navbar/Navbar'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      <h1 className='text-white' >hello everyone</h1>
-      <WrapButton href='https://www.google.com/'>Hello</WrapButton>
-      <ShareButton links={[{icon:Twitter},{icon:Banana},{icon:LandPlot},{icon:Cloud}]}>Share</ShareButton>
+      <Navbar />
+      
     </div>
   )
 }
---
+++ src/components/navbar/NavPart2.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavPart2 = () => {
+  return (
+    <div>NavPart2</div>
+  )
+}
+
+export default NavPart2
